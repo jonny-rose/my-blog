7a06794221edcebe83a2e861cb8de45ca2919041
Jonny Rose <jonny_rose44@yahoo.com>
2020-10-11 14:24:25 +0300

    added local database to the blog and used it to render the posts

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const truncate = require("truncate");
-
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -20,8 +14,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/my-blog", { useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
 app.get("/", (req, res) => {
-  res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
+  
+  Post.find({}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
+    }
+  })  
 })
 
 app.get("/about", (req, res) => {
@@ -36,37 +46,38 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
+const postSchema = new mongoose.Schema ({
+  title: {
+    type: String,
+    required: true
+  },
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 app.post("/compose", (req, res) => {
-  const newPost = {
+  
+    const newPost = new Post ({
     title: req.body.newTitle,
     content: req.body.newText
-  }
-  posts.push(newPost)
-  res.redirect("/")
-})
+  })
+  newPost.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });  
+});
 
-app.get("/posts/:postName", (req, res) => {
-    posts.forEach((post) => {
-      if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
+app.get("/posts/:postId", (req, res) => {
+    const requestedPostId = req.params.postId;
+    Post.findOne({_id: requestedPostId}, function(err, post) {
+      
         return res.render("post", {title: post.title, content: post.content})
-      }
-    })
-})
+      
+    });
+});
 
-// app.get("/posts/:postName", (req, res) => {
-   
-//   let postIndex = _.findIndex(posts, (post) => {
-//     return _.lowerCase(post.title) === _.lowerCase(req.params.postName)
-//   })
-  
-//   if (postIndex !== -1) {
-//     let post = posts[postIndex];
-//     console.log(post);
-//     return res.render("post", {post})
-//   } else {
-//     console.log("error: title not found")
-//   }
-// })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
